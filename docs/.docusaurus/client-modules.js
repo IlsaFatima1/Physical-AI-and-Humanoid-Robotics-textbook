c2850987f8a66cf0c4c046a19ac98b19f3a2d79d
hashir <hashirattari73@gmail.com>
2025-12-22 21:47:24 +0500

    Stop tracking build and .docusaurus folders in root and docs

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("D:\\gemini_cli\\hackathon1\\Physical-AI-and-Humanoid-Robotics-textbook\\docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\gemini_cli\\hackathon1\\Physical-AI-and-Humanoid-Robotics-textbook\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\gemini_cli\\hackathon1\\Physical-AI-and-Humanoid-Robotics-textbook\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\gemini_cli\\hackathon1\\Physical-AI-and-Humanoid-Robotics-textbook\\docs\\src\\css\\custom.css"),
-];
